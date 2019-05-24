7a0449b78b6207d52ba812b693896fe8ca032c02
Anthony Gubler <agubler@sitepen.com>
2019-05-24 14:11:06 +0100

    todomvc example

---
+++ src/main.tsx
@@ -0,0 +1,7 @@
+import renderer from '@dojo/framework/widget-core/vdom';
+import { tsx } from '@dojo/framework/widget-core/tsx';
+
+import App from './App';
+
+const r = renderer(() => <App />);
+r.mount();
